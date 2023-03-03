8e98502c4859c0367082624f64e54045fdedc867
Kevin Halliday <halliday.kevin7@gmail.com>
2023-03-03 12:37:02 -0500

    Add global count and attestation index.
    Inidices help with pagination, as the graph allows a max skip arg or
    An incrementing index allows us to view any slice of attestations
    with a "where index less than" query.

--- src/attestations.ts
+++ src/attestations.ts
@@ -1,5 +1,18 @@
-import { AttestationCreated as AttestationCreatedEvent } from "../generated/AttestationStation/AttestationStation"
-import { Attestation } from "../generated/schema"
+import { BigInt } from '@graphprotocol/graph-ts'
+import { AttestationCreated as AttestationCreatedEvent } from '../generated/AttestationStation/AttestationStation'
+import { Attestation, Global } from '../generated/schema'
+
+function global(): Global {
+  let g = Global.load('global')
+
+  if (g == null) {
+    g = new Global('global')
+    g.count = BigInt.zero()
+  }
+
+  return g
+}
+
 
 export function handleAttestationCreated(event: AttestationCreatedEvent): void {
   let attestation = new Attestation(
@@ -9,14 +22,21 @@ export function handleAttestationCreated(event: AttestationCreatedEvent): void {
       .concat(event.logIndex.toString()),
   )
 
+  let g = global()
+
   attestation.creator = event.params.creator
   attestation.about = event.params.about
   attestation.key = event.params.key
   attestation.val = event.params.val
+
+  // zero indexed
+  attestation.index = g.count
+  g.count = g.count.plus(BigInt.fromI32(1))
 
   attestation.blockNumber = event.block.number
   attestation.blockTimestamp = event.block.timestamp
   attestation.transactionHash = event.transaction.hash
 
   attestation.save()
+  g.save()
 }
