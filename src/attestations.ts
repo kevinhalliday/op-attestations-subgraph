0cdc637b4694aecb89f624c9888167ae35cc26b5
Kevin Halliday <halliday.kevin7@gmail.com>
2023-03-03 15:40:17 -0500

    Add account & key level counts and indices.
    Tracking counts and indices for keys allows us to use the graph for more
    complete pagination. It's a little hacky but what can ya do

--- src/attestations.ts
+++ src/attestations.ts
@@ -1,18 +1,19 @@
 import { BigInt } from '@graphprotocol/graph-ts'
 import { AttestationCreated as AttestationCreatedEvent } from '../generated/AttestationStation/AttestationStation'
-import { Attestation, Global } from '../generated/schema'
+import { Attestation, Count } from '../generated/schema'
 
-function global(): Global {
-  let g = Global.load('global')
+function countFor(id: string): Count {
+  let c = Count.load(id)
 
-  if (g == null) {
-    g = new Global('global')
-    g.count = BigInt.zero()
+  if (c == null) {
+    c = new Count(id)
+    c.count = BigInt.zero()
   }
 
-  return g
+  return c
 }
 
+const one = BigInt.fromI32(1)
 
 export function handleAttestationCreated(event: AttestationCreatedEvent): void {
   let attestation = new Attestation(
@@ -22,21 +23,55 @@ export function handleAttestationCreated(event: AttestationCreatedEvent): void {
       .concat(event.logIndex.toString()),
   )
 
-  let g = global()
+  let creator = event.params.creator
+  let about = event.params.about
+  let key = event.params.key
 
-  attestation.creator = event.params.creator
-  attestation.about = event.params.about
-  attestation.key = event.params.key
+  attestation.creator = creator
+  attestation.about = about
+  attestation.key = key
   attestation.val = event.params.val
-
-  // zero indexed
-  attestation.index = g.count
-  g.count = g.count.plus(BigInt.fromI32(1))
-
   attestation.blockNumber = event.block.number
   attestation.blockTimestamp = event.block.timestamp
   attestation.transactionHash = event.transaction.hash
 
+  let globalCount = countFor('global')
+  let creatorCount = countFor(creator.toHex())
+  let aboutCount = countFor(about.toHex())
+  let keyCount = countFor(key.toHex())
+  let keyAndAboutCount = countFor(
+    key
+      .toHex()
+      .concat('-')
+      .concat(about.toHex()),
+  )
+  let keyAndCreatorCount = countFor(
+    key
+      .toHex()
+      .concat('-')
+      .concat(creator.toHex()),
+  )
+
+  // zero indexed
+  attestation.index = globalCount.count
+  attestation.indexForCreator = creatorCount.count
+  attestation.indexForAbout = aboutCount.count
+  attestation.indexForKey = keyCount.count
+  attestation.indexForKeyAndAbout = keyAndAboutCount.count
+  attestation.indexForKeyAndCreator = keyAndCreatorCount.count
+
+  globalCount.count = globalCount.count.plus(one)
+  creatorCount.count = creatorCount.count.plus(one)
+  aboutCount.count = aboutCount.count.plus(one)
+  keyCount.count = keyCount.count.plus(one)
+  keyAndAboutCount.count = keyAndAboutCount.count.plus(one)
+  keyAndCreatorCount.count = keyAndCreatorCount.count.plus(one)
+
   attestation.save()
-  g.save()
+  globalCount.save()
+  creatorCount.save()
+  aboutCount.save()
+  keyCount.save()
+  keyAndAboutCount.save()
+  keyAndCreatorCount.save()
 }
